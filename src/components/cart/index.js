1e9a3656e6a00b1aebc5801d30c96fcbd29a01cd
zhaoxiangtao <sylvenas@outlook.com>
2022-02-22 10:37:55 +0800

    feat: resume

--- src/components/cart/index.js
+++ src/components/cart/index.js
@@ -1,59 +1,72 @@
 import React, { memo, useRef } from "react";
-import { Link } from 'gatsby';
-import { media } from '@utils/theme';
+import { Link } from "gatsby";
+import { media } from "@utils/theme";
 
 const Cart = ({ node }) => {
   const imgName = node.frontmatter.img.substring(6, 16);
   const imgType = node.frontmatter.img.substring(17);
 
-  const svgRef = useRef()
-  const picRef = useRef()
+  const svgRef = useRef();
+  const picRef = useRef();
 
   const handleImgLoad = () => {
     setTimeout(() => {
       svgRef.current.style.opacity = 0;
       picRef.current.style.opacity = 1;
-    })
-  }
+    });
+  };
 
-  if (node.frontmatter.secert) return null
+  if (node.frontmatter.secert) return null;
 
   return (
     <li
       css={{
-        width: '100%',
+        width: "100%",
         minHeight: 500,
-        listStyleType: 'none',
-        [media.size('medium')]: {
-          width: '50%',
+        listStyleType: "none",
+        [media.size("medium")]: {
+          width: "50%",
         },
 
-        [media.greaterThan('large')]: {
-          width: '30.303%',
+        [media.greaterThan("large")]: {
+          width: "30.303%",
           //float: 'left',
-          display: 'inline-block',
-          margin: '0 1.515% 1.875em',
+          display: "inline-block",
+          margin: "0 1.515% 1.875em",
         },
       }}
     >
-      <Link
-        to={node.fields.slug}>
-        <div css={{ height: 250, width: '100%', overflow: 'hidden', position: 'relative' }}>
+      <Link to={node.fields.slug}>
+        <div
+          css={{
+            height: 250,
+            width: "100%",
+            overflow: "hidden",
+            position: "relative",
+            borderRadius: 3,
+          }}
+        >
           <img
             src={`/svg/${imgName}.svg`}
             css={{
-              minHeight: '250px',
-              maxWidth: '100%',
-              position: 'relative',
-              transition: 'opacity .3s',
+              minHeight: "250px",
+              maxWidth: "100%",
+              position: "relative",
+              transition: "opacity .3s",
             }}
             ref={svgRef}
           />
           <picture>
-            <source media="(min-width: 900px)"
-              srcSet={`/img/${imgName}_lg_1x.webp 1x,/img/${imgName}_lg_2x.webp 2x`} type="image/webp" />
-            <source media="(max-width: 900px)"
-              srcSet={`/img/${imgName}_md_1x.webp 1x,/img/${imgName}_md_2x.webp 2x`} type="image/webp" />
+            <source
+              media="(min-width: 900px)"
+              srcSet={`/img/${imgName}_lg_1x.webp 1x,/img/${imgName}_lg_2x.webp 2x`}
+              type="image/webp"
+            />
+            <source
+              media="(max-width: 900px)"
+              srcSet={`/img/${imgName}_md_1x.webp 1x,/img/${imgName}_md_2x.webp 2x`}
+              type="image/webp"
+            />
             <img
               ref={picRef}
               onLoad={handleImgLoad}
@@ -62,74 +75,78 @@ const Cart = ({ node }) => {
               type={`image/${imgType}`}
               alt="image"
               css={{
-                minHeight: '250px',
-                width: '100%',
-                position: 'absolute',
+                minHeight: "250px",
+                width: "100%",
+                position: "absolute",
                 top: 0,
                 left: 0,
                 opacity: 1,
-                transition: 'opacity .3s',
+                transition: "opacity .3s",
               }}
-              description="true" />
+              description="true"
+            />
           </picture>
         </div>
       </Link>
-      <h2 css={{
-        fontFamily: '"brandon-grotesque", sans-serif',
-        fontSize: 10,
-        letterSpacing: '.1em',
-        color: '#7e8890',
-        textTransform: 'uppercase',
-        fontWeight: 700,
-        marginBottom: 15,
-        marginTop: 44,
-        textAlign: 'center',
-        cursor: 'pointer',
-        ':hover': {
-          color: '#323a45',
-        },
-      }}>
+      <h2
+        css={{
+          fontFamily: '"brandon-grotesque", sans-serif',
+          fontSize: 10,
+          letterSpacing: ".1em",
+          color: "#7e8890",
+          textTransform: "uppercase",
+          fontWeight: 700,
+          marginBottom: 15,
+          marginTop: 44,
+          textAlign: "center",
+          cursor: "pointer",
+          ":hover": {
+            color: "#323a45",
+          },
+        }}
+      >
         <a href={`/categories.html#${node.frontmatter.categories}`}>
-          {node.frontmatter.categories ? node.frontmatter.categories : ''}
+          {node.frontmatter.categories ? node.frontmatter.categories : ""}
         </a>
       </h2>
-      <Link
-        to={node.fields.slug}
-      >
-        <h1 css={{
-          fontFamily: '"brandon-grotesque", sans-serif',
-          fontSize: 18,
-          lineHeight: '27px',
-          letterSpacing: '.065em',
-          color: '#fc3768',
-          textTransform: 'uppercase',
-          fontWeight: 700,
-          maxWidth: 270,
-          margin: '0 auto',
-          textAlign: 'center',
-          cursor: 'pointer',
-          ':hover': {
-            color: '#b1032e',
-          },
-        }}>
+      <Link to={node.fields.slug}>
+        <h1
+          css={{
+            fontFamily: '"brandon-grotesque", sans-serif',
+            fontSize: 18,
+            lineHeight: "27px",
+            letterSpacing: ".065em",
+            color: "#fc3768",
+            textTransform: "uppercase",
+            fontWeight: 700,
+            maxWidth: 270,
+            margin: "0 auto",
+            textAlign: "center",
+            cursor: "pointer",
+            ":hover": {
+              color: "#b1032e",
+            },
+          }}
+        >
           {node.frontmatter.title}
         </h1>
       </Link>
-      <p css={{
-        fontSize: 14,
-        lineHeight: '27px',
-        color: '#333c4e',
-        padding: '0 25px',
-        textAlign: 'center',
-        marginTop: '30px',
-        width: '85%',
-        margin: '40px auto',
-      }}
+      <p
+        css={{
+          fontSize: 14,
+          lineHeight: "27px",
+          color: "#333c4e",
+          padding: "0 25px",
+          textAlign: "center",
+          marginTop: "30px",
+          width: "85%",
+          margin: "40px auto",
+        }}
       >
         {node.excerpt}
       </p>
     </li>
-  )
-}
+  );
+};
 
-export default memo(Cart)
+export default memo(Cart);
--- src/pages/resume/index.js
+++ src/pages/resume/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import Layout from '@components/layout';
-import Container from '@components/container';
+import React from "react";
+import Layout from "@components/layout";
+import Container from "@components/container";
 
-import "./index.less"
+import "./index.less";
 
 class Resume extends React.Component {
   constructor() {
     super();
-    this.state = { active: 0 };
+    this.state = { active: 1 };
     this.handleClick = this.handleClick.bind(this);
   }
   handleClick(idx) {
@@ -17,33 +17,63 @@ class Resume extends React.Component {
     const { active } = this.state;
     return (
       <Layout location={this.props.location}>
-        <Container css={{
-          position: 'relative',
-        }}>
-          <div css={{
-            margin: '2em auto 1em',
-            width: '100%',
-          }}>
+        <Container
+          css={{
+            position: "relative",
+          }}
+        >
+          <div
+            css={{
+              margin: "2em auto 1em",
+              width: "100%",
+            }}
+          >
             <h2 className="resume-title">About</h2>
             <p className="resume-p">
-              Tao Zhao lives in HangZhou, ZheJiang. He likes basketball, books,
-              games and travel.
+              I live in HangZhou, ZheJiang, and like basketball, books, games
+              and travel.
             </p>
             <br />
-            <p className="resume-p">Math is my next programming language</p>
+            <p className="resume-p">Math is my next programming language.</p>
             <br />
-            <p className="resume-p"><b>Keywords: </b>
-              <span className="keywords"><a href="/categories.html#functional">Functional Programming</a></span>、
-              <span className="keywords"><a href="/categories.html#Async">Async Programming</a></span>、
-              <span className="keywords"><a href="/blog/2017/11/20/FP5-closure.html">Closure</a></span>、
-              <span className="keywords"><a href="/blog/2018/09/02/FP14-Monad-1.html">Functor/Monad</a></span>、
-              <span className="keywords"><a href="/categories.html#composing%20software">Function Compose</a></span>、
-              <span className="keywords"><a href="/categories.html#React">React/Redux</a></span>
+            <p className="resume-p">
+              <b>Keywords: </b>
+              <span className="keywords">
+                <span>Functional Programming</span>
+              </span>
+              ,
+              <span className="keywords">
+                <span>Async Programming</span>
+              </span>
+              ,
+              <span className="keywords">
+                <span>Node.js/Koa/MongoDB</span>
+              </span>
+              ,
+              <span className="keywords">
+                <span>React/Redux/Jotai</span>
+              </span>
+              ,
+              <span className="keywords">
+                <span>React-Native</span>
+              </span>
+              ,
+              <span className="keywords">
+                <span>Next.js</span>
+              </span>
+              ,
+              <span className="keywords">
+                <span>Tailwindcss</span>
+              </span>
+              .
             </p>
             <Love />
             <p className="resume-p">
-              Mello is currently a Frontend developer engineer and manager of the Frontend Platform team at
-              <a href="http://netease.com" target="_blank"> NetEase</a>. He originally joined the Listings team at NetEase in Apr 2017, tasked to revamp the event details pages from an archaic MySpace-style design to a modern, fully responsive layout.
+              I am a senior front-end developer with 8 years of experience, and
+              a code-acholic who invested a lot of time developing and
+              maintaining several projects in the open-source community,
+              developing and maintaining several projects. I love to summarize
+              and share my knowledge, and have written almost 100 blogs.
             </p>
           </div>
           <Posts handleClick={this.handleClick} active={active} />
@@ -58,90 +88,121 @@ class Resume extends React.Component {
 
 const Love = () => (
   <div className="love-container">
-    <img src="https://p1.music.126.net/cApgVVmtcKYFyxunEmHCkA==/109951164540902241.jpg" className="resume-love" />
+    <img
+      src="https://p1.music.126.net/cApgVVmtcKYFyxunEmHCkA==/109951164540902241.jpg"
+      className="resume-love"
+    />
   </div>
 );
 
 const Posts = ({ active, handleClick }) => (
-  <div css={{
-    borderTop: '1px solid #efefef',
-    display: 'flex',
-    justifyContent: 'center',
-    letterSpacing: '1px',
-    textAlign: 'center',
-    fontSize: '12px',
-    fontWeight: 600,
-  }}>
-    <a css={{
-      cursor: 'pointer',
-      lineHeight: '52px',
-      marginRight: 60,
-      borderTop: active == 0 ? '1px solid #262626' : 'none',
-      marginTop: active == 0 ? -1 : 0,
-      color: active === 0 ? '#262626' : '#999',
+  <div
+    css={{
+      borderTop: "1px solid #efefef",
+      display: "flex",
+      justifyContent: "center",
+      letterSpacing: "1px",
+      textAlign: "center",
+      fontSize: "12px",
+      fontWeight: 600,
     }}
-      onClick={handleClick.bind(null, 0)}>
-      BIO
-    </a>
-    <a css={{
-      cursor: 'pointer',
-      lineHeight: '52px',
-      marginRight: 60,
-      borderTop: active == 1 ? '1px solid #262626' : 'none',
-      marginTop: active == 1 ? -1 : 0,
-      color: active === 1 ? '#262626' : '#999',
-    }}
+  >
+    <a
+      css={{
+        cursor: "pointer",
+        lineHeight: "52px",
+        marginRight: 60,
+        borderTop: active == 1 ? "1px solid #262626" : "none",
+        marginTop: active == 1 ? -1 : 0,
+        color: active === 1 ? "#262626" : "#999",
+      }}
       onClick={handleClick.bind(null, 1)}
     >
-      PROJECTS</a>
-    <a css={{
-      cursor: 'pointer',
-      lineHeight: '52px',
-      marginRight: 60,
-      borderTop: active == 2 ? '1px solid #262626' : 'none',
-      marginTop: active == 2 ? -1 : 0,
-      color: active === 2 ? '#262626' : '#999',
-    }}
+      PROJECTS
+    </a>
+    <a
+      css={{
+        cursor: "pointer",
+        lineHeight: "52px",
+        marginRight: 60,
+        borderTop: active == 0 ? "1px solid #262626" : "none",
+        marginTop: active == 0 ? -1 : 0,
+        color: active === 0 ? "#262626" : "#999",
+      }}
+      onClick={handleClick.bind(null, 0)}
+    >
+      BIO
+    </a>
+    <a
+      css={{
+        cursor: "pointer",
+        lineHeight: "52px",
+        marginRight: 60,
+        borderTop: active == 2 ? "1px solid #262626" : "none",
+        marginTop: active == 2 ? -1 : 0,
+        color: active === 2 ? "#262626" : "#999",
+      }}
       onClick={handleClick.bind(null, 2)}
     >
-      BOOKS</a>
+      BOOKS
+    </a>
   </div>
 );
 
 const WorkInfo = () => (
-  <div css={{
-    margin: '1em auto 5em',
-  }}>
-    {/* <h2 className="resume-title">Bio</h2> */}
+  <div
+    css={{
+      margin: "1em auto 5em",
+    }}
+  >
     <div className="timeline">
       <div className="title">
         <div className="year work">
-          <span className="work-date">Apr 2017 - Now</span> Fontend Developer @ <a href="http://netease.com" target="_blank">NetEase</a>
+          <span className="work-date">Apr 2017 - Now</span> Fontend Developer @{" "}
+          <a href="http://netease.com" target="_blank">
+            NetEase
+          </a>
         </div>
-        <div className="year work">
-          <span className="work-date">May 2015 - Apr 2017</span> Web Developer @ <a href="https://www.bilibili.com/" target="_blank">Bilibili</a>
-        </div>
-        {/* <div className="year work">
-          <span className="work-date">July 2010 - May 2014</span> Software Developer @ <a href="http://sap.com" target="_blank">tongyan</a>
-        </div> */}
       </div>
     </div>
   </div>
 );
 const Projects = () => (
-  <div css={{
-    margin: '1em auto 1em',
-  }}>
+  <div
+    css={{
+      margin: "1em auto 1em",
+    }}
+  >
     <h2 className="resume-title">Projects</h2>
-    <span className="resume-sub-title">react-magic</span>
+    <span className="resume-sub-title">
+      <a href="https://github.com/lit-forest/react-magic">react-magic</a>
+    </span>
     <p className="resume-p">
-      A collection of magic animations for react components. Although React provides a way to implement arbitrary animations, it is not an easy task to do it, even for simple animations. That's where react-magic package comes in. It supports inline styles work with
-      <a href="https://github.com/Khan/aphrodite" target="_blank"> aphrodite</a>. Most animations was implemented base on
-      <a href="https://github.com/miniMAC/magic" target="_blank"> magic</a>.
+      A collection of magic animations for react components. Although React
+      provides a way to implement arbitrary animations, it is not an easy task
+      to do it, even for simple animations. That's where react-magic package
+      comes in. It supports inline styles work with aphrodite. Most animations
+      was implemented base on magic.
     </p>
-    <span className="resume-sub-title">leaflet.migrationLayer</span>
+    <span className="resume-sub-title">
+      <a href="https://github.com/lit-forest/leaflet.migrationLayer">
+        leaflet.migrationLayer
+      </a>
+    </span>
     <p className="resume-p">
-      leafet.migrationLayer is used to show migration data such as population,flight,vehicle,traffic and so on.Data visualization on map.
+      leafet.migrationLayer is used to show migration data such as
+      population,flight,vehicle,traffic and so on.Data visualization on map.
+    </p>
+    <span className="resume-sub-title">
+      <a href="https://github.com/Sylvenas/tailwindcss-animation-property">
+        tailwindcss-animation-property
+      </a>
+    </span>
+    <p className="resume-p">
+      A plugin that provides utilities for animation property. Not only does the
+      plugin provide the usual animation properties, these also support dynamic
+      writing using JIT mode, even you can add new properties by using
+      configuration.
     </p>
   </div>
 );
@@ -152,21 +213,26 @@ const BOOKS = [
   "https://p6.music.126.net/obj/wo3DlcOGw6DClTvDisK1/10032110426/ffb5/d4a1/6653/75eb0fd16fa52d345cb092ba2e05b2ba.jpeg",
   "https://p6.music.126.net/obj/wo3DlcOGw6DClTvDisK1/10032111391/9f78/f982/844b/1a1a3b59bf56f00d3c55da7eb44cd5a8.jpeg",
   "https://p6.music.126.net/obj/wo3DlcOGw6DClTvDisK1/10032111405/406f/d701/702e/0a24e80ff552f11261856e4e2f1542f5.jpeg",
-  "https://p6.music.126.net/obj/wo3DlcOGw6DClTvDisK1/10032112256/fbd0/984a/e9e9/d40ed4aaa7dc063cbea568c7ef5ed33e.jpeg",
 ];
 
 const Books = () => (
-  <div css={{
-    display: 'flex',
-    flexWrap: 'wrap',
-  }}>
-    {BOOKS.map((book, idx) => <img
-      key={idx}
-      src={book} css={{
-        height: 200,
-        width: 150,
-        margin: 10,
-      }} />)}
+  <div
+    css={{
+      display: "flex",
+      flexWrap: "wrap",
+    }}
+  >
+    {BOOKS.map((book, idx) => (
+      <img
+        key={idx}
+        src={book}
+        css={{
+          height: 200,
+          width: 150,
+          margin: 10,
+        }}
+      />
+    ))}
   </div>
 );
 
